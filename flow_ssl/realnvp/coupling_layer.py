c1d04b75058c51062b6d02b285aa297a86385a17
nkandpa2 <nkandpa2@gmail.com>
2020-08-18 17:38:59 -0400

    Update coupling_layer.py

--- flow_ssl/realnvp/coupling_layer.py
+++ flow_ssl/realnvp/coupling_layer.py
@@ -139,8 +139,7 @@ class CouplingLayer(CouplingLayerBase):
         # Pavel: reuse Marc's ResNet block?
         self.st_net = ResNet(in_channels, mid_channels, 2 * in_channels,
                              num_blocks=num_blocks, kernel_size=3, padding=1,
-                             double_after_norm=(self.mask.type == MaskType.CHECKERBOARD),
-                             init_zeros=init_zeros)
+                             double_after_norm=(self.mask.type == MaskType.CHECKERBOARD))
 
         # Learnable scale for s
         self.rescale = nn.utils.weight_norm(Rescale(in_channels))
